720af9acfbcdebcc8a11fff9bbd97dd590673582
Vivien <vivien.tan@keysight.com>
2022-11-18 11:32:25 +0800

    add checking current branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -148,10 +148,13 @@ echo "[+] git diff-index:"
 # git diff-index : to avoid doing the git commit failing if there are no changes to be commit
 git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
+echo "[+] Checking branches"
+git branch 
+
 echo "[+] Pushing git commit"
 # --set-upstream: sets de branch when pushing to a branch that does not exist
 # git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
-git push "$GIT_CMD_REPOSITORY" "$TARGET_BRANCH"
+git push "$GIT_CMD_REPOSITORY" 
 
 echo "Creating a pull request"
 gh pr create -t $TARGET_BRANCH \
