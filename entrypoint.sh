c77e89ed21b5d7a0e5acbd9337f3ccd3d81c371c
Vivien <vivien.tan@keysight.com>
2022-11-21 16:57:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -176,14 +176,14 @@ echo "[+] Pushing git commit"
 git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
 
 echo "[+] Creating a pull request"
-CLONE_DIR_PR=$(mktemp -d)
+# CLONE_DIR_PR=$(mktemp -d)
 
-export GITHUB_TOKEN=$GITHUB_TOKEN
-git config --global user.email "$USER_EMAIL"
-git config --global user.name "$USER_NAME"
+# export GITHUB_TOKEN=$GH_TOKEN
+# git config --global user.email "$USER_EMAIL"
+# git config --global user.name "$USER_NAME"
 
-git clone --branch $TARGET_BRANCH "https://$GITHUB_TOKEN@github.com/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR_PR"
-cd "$CLONE_DIR_PR"
+# git clone --branch $TARGET_BRANCH "https://$GITHUB_TOKEN@github.com/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR_PR"
+# cd "$CLONE_DIR_PR"
 
 gh pr create --title $TARGET_BRANCH \
             --body $TARGET_BRANCH \
